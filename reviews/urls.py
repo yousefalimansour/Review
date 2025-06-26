5dd5fff86ad8ea5ffab2b9380bb84b1239866881
Yousef Ali <you@example.com>
2025-06-26 16:36:38 +0300

    final

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("",views.ReviewView.as_view()),
+    path("",views.ReviewCreateView.as_view()),
     path("thank-you",views.ThankyouView.as_view()),
-    path("reviews",views.ReviewsListView.as_view()),
-    path("review/<int:id>",views.SingleReiewView.as_view())
+    path("reviews",views.ReviewsList2View.as_view()),
+    path("review/<int:pk>",views.SingleDetailReiewView.as_view())
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -3,8 +3,37 @@ from django.shortcuts import render
 from django.views import View 
 from .forms import ReviewForm
 from django.views.generic.base import TemplateView
+from django.views.generic import ListView ,DetailView
+from django.views.generic.edit import FormView ,CreateView
 from .models import Review
 # Create your views here.
+
+
+
+class ReviewCreateView(CreateView):
+    model = Review
+    form_class = ReviewForm
+    template_name = "reviews/review.html"
+    success_url = "/thank-you"
+
+class ReviewFormView(FormView):
+    form_class = ReviewForm
+    template_name = "reviews/review.html"
+    success_url = "/thank-you"
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+    
+
+
+class ReviewsList2View(ListView):
+    template_name = "reviews/reviews_list.html"
+    model = Review
+    context_object_name = "reviews"
+
+class SingleDetailReiewView(DetailView):
+    template_name = "reviews/single_review.html"
+    model = Review
 
 class ReviewView(View):
     def get(self,request):
@@ -23,12 +52,9 @@ class ReviewView(View):
             "form":form
         })
 
-
-
 class ThankyouView(TemplateView):
     template_name = "reviews/thank_you.html"
     
-
 class ReviewsListView(TemplateView):
     template_name = "reviews/reviews_list.html"
 
@@ -38,8 +64,6 @@ class ReviewsListView(TemplateView):
         context["reviews"] = reviews 
         return context
     
-
-
 class SingleReiewView(TemplateView):
     template_name = "reviews/single_review.html"
 
@@ -50,8 +74,8 @@ class SingleReiewView(TemplateView):
         context["review"] = selected_review
         return context
     
-
 def review(request):
+
     if request.method =="POST":
         # enterd_name = request.POST["username"]
         form=ReviewForm(request.POST)
