e5267ab7141d5165c6c2402aee6eee1f9d33bc3d
yousefalimansour <yousefmorad@std.mans.edu.eg>
2025-06-27 17:29:26 +0300

    FINAL

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.CreateProfileView.as_view())
+    path("", views.CreateProfileView.as_view()),
+    path("all", views.CreateProfileListView.as_view()),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.views import View
+from django.views.generic import View
 from django.http import HttpResponseRedirect
 from .forms import ProfileForm
 from .models import UserProfile
+from django.views.generic import ListView , CreateView
 # Create your views here.
 
 # def store_file(file):
@@ -10,17 +11,32 @@ from .models import UserProfile
 #         for chunk in file.chunks():
 #             dest.write(chunk)
 
-class CreateProfileView(View):
-    def get(self, request):
-        form = ProfileForm()
-        return render(request, "profiles/create_profile.html",{
-            "form":form
-        })
 
-    def post(self, request):
-        # store_file(request.FILES["image"])
-        submitted_form = ProfileForm(request.POST,request.FILES)
-        if submitted_form.is_valid():
-            profile = UserProfile(image = request.FILES["user_image"])
-            profile.save()
-            return HttpResponseRedirect("/profiles")
+class CreateProfileView(CreateView):
+    model = UserProfile
+    template_name = "profiles/create_profile.html"
+    success_url = "/profiles"
+    fields = "__all__"
+
+class CreateProfileListView(ListView):
+    model = UserProfile
+    template_name = "profiles/create_listprofile.html"
+    context_object_name = "profiles"
+
+
+
+
+# class CreateProfileView(View):
+#     def get(self, request):
+    #     form = ProfileForm()
+    #     return render(request, "profiles/create_profile.html",{
+    #         "form":form
+    #     })
+
+    # def post(self, request):
+    #     # store_file(request.FILES["image"])
+    #     submitted_form = ProfileForm(request.POST,request.FILES)
+    #     if submitted_form.is_valid():
+    #         profile = UserProfile(image = request.FILES["user_image"])
+    #         profile.save()
+    #         return HttpResponseRedirect("/profiles")
